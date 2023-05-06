918730cedcfb7c2b2b5db2fc59f40bd32235a7bf
tnioa <hortanio@hotmail.com>
2023-05-06 17:56:31 -0300

    Correção de smells

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-import express from 'express';
-import cors from 'cors';
-import { json, urlencoded } from 'body-parser';
+const express = require('express');
+var cors = require('cors')
+const bodyParser = require('body-parser');
 const app = express()
 app.use(cors())
 const port = 3000
 
-app.use(json()) // for parsing application/json
-app.use(urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 app.get('/check', (req, res) => {
     console.log(req.body);
