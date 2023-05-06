8670477906ceb248b49c44f0488707288ab5fa42
tnioa <hortanio@hotmail.com>
2023-05-06 18:51:04 -0300

    Correção de smell

--- index.js
+++ index.js
@@ -9,25 +9,30 @@ app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 app.get('/check', (req, res) => {
-    console.log("Query:");
-    console.log(req.query);
+    logContent(req);
+
+    // necessário converter de dotCase to snakeCase
     const jsonQuery = Object.entries(req.query).reduce((x,[k,v]) => (x[toSnake(k)]=v) && x, {})
-    console.log(jsonQuery.hub_challenge);
-    console.log("Body:");
-    console.log(req.body);
+
     res.status(200).send(jsonQuery.hub_challenge)
 });
 
 app.post('/check', (req, res) => {
-    console.log("Query:");
-    console.log(req.query);
+    logContent(req);
+
+    // necessário converter de dotCase to snakeCase
     const jsonQuery = Object.entries(req.query).reduce((x,[k,v]) => (x[toSnake(k)]=v) && x, {})
-    console.log(jsonQuery.hub_challenge);
-    console.log("Body:");
-    console.log(req.body);
+
     res.status(200).send(jsonQuery.hub_challenge)
 });
 
+function logContent(req) {
+    console.log("Query:");
+    console.log(req.query);
+
+    console.log("Body:");
+    console.log(req.body);
+}
 
 const toSnake = s => s.replace(".", "_")
 
